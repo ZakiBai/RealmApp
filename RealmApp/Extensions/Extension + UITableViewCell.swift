b9c6ec3238d11607eefe25f2fbf987654b845376
Zaki <menza.zm@gmail.com>
2023-06-01 14:48:39 +0600

    extension to UITableViewCell configure

--- RealmApp/Extensions/Extension + UITableViewCell.swift
+++ RealmApp/Extensions/Extension + UITableViewCell.swift
@@ -5,4 +5,26 @@
 //  Created by Zaki on 01.06.2023.
 //
 
-import Foundation
+import UIKit
+
+extension UITableViewCell {
+    func configure(with taskList: TaskList) {
+        let currentTask = taskList.tasks.filter("isComplete = false")
+        var content = defaultContentConfiguration()
+        
+        content.text = taskList.title
+        
+        if taskList.tasks.isEmpty {
+            content.secondaryText = "0"
+            accessoryType = .none
+        } else if currentTask.isEmpty {
+            content.secondaryText = nil
+            accessoryType = .checkmark
+        } else {
+            content.secondaryText = currentTask.count.formatted()
+            accessoryType = .none
+        }
+        
+        contentConfiguration = content
+    }
+}
--- RealmApp/ViewControllers/TaskListViewController.swift
+++ RealmApp/ViewControllers/TaskListViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+import RealmSwift
 
-class TaskListViewController: UITableViewController {
+final class TaskListViewController: UITableViewController {
 
+    private var taskLists: Results<TaskList>!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let addBarButton = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(addButtonPressed)
+        )
+        
+        navigationItem.rightBarButtonItem = addBarButton
+        navigationItem.leftBarButtonItem = editButtonItem
+        
+        //methods
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
     }
 
+    // MARK: UITableViewDataSource
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        taskLists.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "taskListCell", for: indexPath)
+        let taskList = taskLists[indexPath.row]
+        cell.configure(with: taskList)
+        return cell
+    }
+    
+    // UITableViewDelegate
+    
+    
+    @objc func addButtonPressed(){
+        // method
+    }
+    
 
 }
 
